d596c2de40fa8aa53a634c51abe49523dd71c01b
Aniko Kovac <nicky@DESKTOP-KIH02FE.localdomain>
2019-08-03 13:18:04 +0200

    gitignore

--- tmx_to_txt_per_language.py
+++
@@ -1,9 +0,0 @@
-from translate.storage.tmx import tmxfile
-
-with open("en-sr.tmx", 'rb') as inputFile:
-	processedFile = tmxfile(inputFile, 'en', 'sr')
-	for entry in processedFile.unit_iter():
-		# English
-		print(entry.getsource())
-		# Serbian
-		print(entry.gettarget())
